e888670dacfe528beddaa33da15c0befbb551df0
birukGetachew <burab1742@gmail.com>
2025-12-27 04:14:32 -0800

    sdf

--- natiportifolio/settings.py
+++ natiportifolio/settings.py
@@ -81,7 +81,11 @@ MIDDLEWARE = [
 # CORS
 # --------------------------------------------------
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 # --------------------------------------------------
