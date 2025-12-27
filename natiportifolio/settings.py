154043907fc0ae10b63a84c4784488e653c62a5b
birukGetachew <burab1742@gmail.com>
2025-12-27 12:32:32 -0800

    sdf

--- natiportifolio/settings.py
+++ natiportifolio/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5500",
-    "https://natiadmin-production.up.railway.app/",
+    "https://natiadmin-production.up.railway.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
